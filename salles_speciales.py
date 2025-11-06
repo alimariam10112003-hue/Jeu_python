b3ae30014b9b6f51b273415893515e804b8b8a3f
Mariam Ali <alimariam10112003@gmail.com>
2025-11-06 17:39:48 +0100

    Création d'un fichier salles spéciales avec la salle de départ et la salle d'arrivée et une fonction vide pour le catalogue des salles (les autres salles)

---
+++ salles_speciales.py
@@ -0,0 +1,40 @@
+from salle import Salle
+
+# Définition de la salle de départ du manoir qui n'a aucun coût, et 4 portes dans les 4 directions
+
+class EntranceHall(Salle):
+
+    def __init__(self):
+        # La salle de départ n'a pas de coût, rareté ou condition de placement.
+        # Elle a des portes dans les 4 directions par défaut dans le jeu original.
+        Salle.__init__(
+            self,
+            nom="Entrance Hall",
+            couleur="bleu",
+            image_path="images/entrance_hall.png", 
+            cout=0,
+            rarete=0,
+            condition_placement="Premiere_Piece", 
+            porte={"N": True, "S": True, "E": True, "O": True},
+            objet=[],
+            effet=None
+        )
+
+# Définition de la salle d'arrivée => effet de victoire 
+
+class Antechamber(Salle):
+    
+    def __init__(self):
+        Salle.__init__(
+            self,
+            nom="Antechamber",
+            couleur="bleu",
+            image_path="images/antechamber.png", 
+            cout=0,
+            rarete=3,
+            condition_placement="Derniere_Piece", 
+            porte={"N": False, "S": True, "E": False, "O": False},
+            objet=[],
+            effet="Victoire"
+        )
+    
