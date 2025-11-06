eb8f8db334f1b2eda5827a25462bc5afe54ca345
Mariam Ali <alimariam10112003@gmail.com>
2025-11-06 17:15:49 +0100

    Correction du commit avec les salle pour laisser que la classe

--- salle.py
+++ salle.py
@@ -9,14 +9,16 @@ class Salle:
     Utilité: regrouper les variables pour décrire une pièce et le traitement associé aux pièces.
     """
     
-    def __init__(self, nom: str, couleur: str,image_path: str, cout: int, rarete: int, porte: dict, objet: list = None, effet: str = None):
+    def __init__(self, nom: str, couleur: str,image_path: str, cout: int, rarete: int, condition_placement: str, porte: dict, objet: list = None, effet: str = None):
         """
         Initialisation d'une salle
         
         * nom: nom de la pièce
         * couleur: couleur de la pièce (jaune,verte,violette,orange,rouge,bleu)
+        * image_path: chemin de l'image de la pièce
         * cout: coût en gemme de de la pièce 
         * rarete: rareté de la pièce (entier entre 0 et 3)
+        * condition_placement: condition sur le placement de la pièce dans le mamoir si besoin
         * porte: porte disponible (ou non) dans la pièce
         * objet: objet contenu dans la pièce
         * effet: effet spécial de la pièce
@@ -24,27 +26,12 @@ class Salle:
         
         self.nom = nom
         self.couleur = couleur
+        self.image_path = image_path
         self.cout = cout
         self.rarete = rarete
+        self.condition_placement = condition_placement
         self.porte = porte
         self.objet = objet if objet is not None else []
         self.effet = effet
-        self.position = None 
 
-    def objets(self):
-        """
-        Détermine aléatoirement si il a des objets ou non dans la pièce.
-        """
-
-        pass
-            
-    def interagir(self, joueur):
-        """
-        Gestion effets spéciaux et intération avec des objets
-        """
-        print(f"Interaction dans la salle: {self.nom} (Couleur: {self.couleur})")
-        pass
-        
-    def __str__(self):
-        """Caractéristique de chaque salle"""
-        return f"Salle: {self.nom} | Couleur: {self.couleur} | Coût: {self.cout_gemmes} | Rareté: {self.rarete}"
+        self.position = None
